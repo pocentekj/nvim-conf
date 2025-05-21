2afb50a78ad0c993236b834c25f21e770920fb7c
Jakub Pocentek <chester@programmer.net>
2025-05-21 06:43:49 +0200

    Initial commit

---
+++ init.lua
@@ -0,0 +1,50 @@
+-- ===============================
+-- Options
+-- ===============================
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.smartindent = true
+vim.opt.number = true
+vim.opt.relativenumber = false
+vim.opt.colorcolumn = "80,120"
+vim.opt.termguicolors = true
+vim.opt.background = "dark"
+vim.opt.wrap = false
+
+-- Filetype-specific indentation
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "c", "html", "css", "javascript", "typescript", "lua", "yaml", "json" },
+  callback = function()
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.tabstop = 2
+  end,
+})
+
+-- ===============================
+-- Lazy.nvim Plugins
+-- ===============================
+require("plugins")
+
+-- ===============================
+-- Keybindings
+-- ===============================
+vim.keymap.set("n", "<Tab>", "<Cmd>BufferLineCycleNext<CR>", { desc = "Next buffer" })
+vim.keymap.set("n", "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", { desc = "Previous buffer" })
+
+-- ===============================
+-- Auto commands
+-- ===============================
+-- Open NERDTree
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    if vim.fn.argc() == 0 then
+      vim.cmd("NERDTree | wincmd p")
+    end
+  end,
+})
+
+-- ===============================
+-- LSP Setup
+-- ===============================
+require("completion")
---
+++ lua/plugins.lua
@@ -0,0 +1,65 @@
+-- ===============================
+-- Lazy.nvim Bootstrap
+-- ===============================
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git", "clone", "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- ===============================
+-- Plugins
+-- ===============================
+require("lazy").setup({
+  -- UI & Navigation
+  { "preservim/nerdtree" },
+  { "jlanzarotta/bufexplorer" },
+  {
+    "akinsho/bufferline.nvim",
+    version = "*",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require("bufferline").setup({
+        options = {
+          mode = "buffers",
+          separator_style = "slant",
+          diagnostics = "nvim_lsp",
+          show_close_icon = false,
+          always_show_bufferline = true,
+          offsets = {
+            { filetype = "NvimTree", text = "File Explorer", padding = 1 },
+          },
+        },
+      })
+    end,
+  },
+
+  -- GIT integration
+  { "lewis6991/gitsigns.nvim" },
+
+  -- LSP & Completion
+  { "neovim/nvim-lspconfig" },
+  { "hrsh7th/nvim-cmp" },
+  { "hrsh7th/cmp-nvim-lsp" },
+  { "L3MON4D3/LuaSnip" },
+  { "onsails/lspkind-nvim" },
+
+  -- Theme
+  {
+    "EdenEast/nightfox.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("nightfox").setup({
+        options = {
+          transparent = true,
+        }
+      })
+      vim.cmd("colorscheme carbonfox")
+    end
+  },
+})
