35c19c28722b00fd39bda5a4cccb287c10caacee
Jakub Pocentek <chester@programmer.net>
2025-05-21 07:40:17 +0200

    Additional config options

--- init.lua
+++ init.lua
@@ -5,12 +5,18 @@ vim.opt.expandtab = true
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
 vim.opt.smartindent = true
-vim.opt.number = true
-vim.opt.relativenumber = false
-vim.opt.colorcolumn = "80,120"
+vim.opt.number = true           -- Always show line numbers
+vim.opt.relativenumber = false  -- Disable relative line numbers
+vim.opt.colorcolumn = "80,120"  -- Vertical rulers
 vim.opt.termguicolors = true
 vim.opt.background = "dark"
-vim.opt.wrap = false
+vim.opt.wrap = false            -- Don't wrap lines
+vim.opt.foldcolumn = "1"        -- Add a bit extra margin to the left
+vim.opt.ruler = true            -- Always show current position
+vim.opt.showmatch = true        -- Show matching brackets when text indicator is over them
+vim.opt.incsearch = true        -- Makes search act like search in modern browsers
+vim.opt.magic = true            -- For regular expressions turn magic on
+vim.opt.lazyredraw = true       -- Don't redraw while executing macros (good performance config)
 
 -- Filetype-specific indentation
 vim.api.nvim_create_autocmd("FileType", {
