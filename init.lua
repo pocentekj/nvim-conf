a3d44d48e5ec641ab14ed3da947978228227125b
Jakub Pocentek <chester@programmer.net>
2025-05-29 15:40:00 +0200

    Ctrl+S to save session

--- init.lua
+++ init.lua
@@ -122,3 +122,9 @@ vim.keymap.set("n", "<leader>zc", "<Cmd>foldclose<CR>", { desc = "Close fold" })
 vim.keymap.set("n", "<leader>zR", "<Cmd>foldtoggle<CR>", { desc = "Toggle fold" })
 -- NERDTree
 vim.keymap.set('n', '<C-n>', ':NERDTreeToggle<CR>', { noremap = true, silent = true })
+-- Session management
+vim.keymap.set('n', '<C-s>', function()
+  vim.cmd('NERDTreeClose')
+  vim.cmd('mksession!')
+end, { noremap = true, silent = true })
+
