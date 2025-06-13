fb2bf10b44add556834df30849faeef7bcbe0d09
Chester <chester@programmer.net>
2025-06-13 22:38:19 +0200

    Adjusted Rust completion settings

--- lua/completion.lua
+++ lua/completion.lua
@@ -69,7 +69,7 @@ lspconfig.rust_analyzer.setup({
   settings = {
     ["rust-analyzer"] = {
       cargo = { allFeatures = true },
-      checkOnSave = { command = "clippy" },
+      checkOnSave = true,
     },
   },
 })
