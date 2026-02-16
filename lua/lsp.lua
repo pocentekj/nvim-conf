fd289a1024cc3074a7674b7d095b20ac6910e03b
Chester <chester@programmer.net>
2026-02-17 00:59:38 +0100

    LSP keybindings

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -14,8 +14,6 @@ do
       mapping = cmp.mapping.preset.insert({
         ["<C-Space>"] = cmp.mapping.complete(),
         ["<CR>"] = cmp.mapping.confirm({ select = true }),
-        ["<Tab>"] = cmp.mapping.select_next_item(),
-        ["<S-Tab>"] = cmp.mapping.select_prev_item(),
         ["<Esc>"] = cmp.mapping.abort(),
       }),
       sources = {
